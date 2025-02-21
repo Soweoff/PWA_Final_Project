fbd649bc3add2cd2412a802167d631b29fbcbc78
Everton Souza Wience <131189665+Soweoff@users.noreply.github.com>
2025-02-20 21:56:13 -0300

    Create index.jsx
    Prova de Recuperacao 3

---
+++ index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+<View className="Home">
+<text className="title">Welcome to PWA App</text>
+  <text className="info">More information about PWA</text>
+</View>
+  );
+}
